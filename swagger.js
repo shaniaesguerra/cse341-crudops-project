abc534d3f403584ad252f384ae32e52cf2e43505
shaniaesguerra <shaniae47@gmail.com>
2026-07-18 11:38:26 -0600

    adding swagger

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Contacts API',
+        description: 'CSE 341: Week 03 Project2 CRUD Operations'
+    },
+    host: 'localhost:3000',
+    schemes: ['http', 'https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+//Generate Swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
